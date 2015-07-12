db17f964177fb4d5eca56e3ba61780f33e728744
Sujith G <sujith3g@gmail.com>
2015-07-13 01:20:41 +0530

    request with access_token

--- epo-ops.js
+++ epo-ops.js
@@ -46,23 +46,31 @@ Epo.prototype.register = function(options, callback) {
       url: urlBuilder.join("/"),
       headers: {
         'Accept': 'application/json',
-        //'Authorization: Bearer '.$token['access_token'],
         'Connection': 'Keep-Alive',
         'Host': 'ops.epo.org',
         'X-Target-URI': 'http://ops.epo.org'
       }
     };
-    if (process.env.DEBUG) console.log('Request to register : ',reqOptions);
+    if (this.token) {
+      if (this.token.expired()) {
+        _this = this;
+        this.token.refresh(function(error, result) {
+          _this.token = result;
+        });
+      }
+      reqOptions["headers"]["Authorization"] = "Bearer " +this.token["token"]["access_token"];
+    }
+    if (process.env.DEBUG) console.log('Request to register : ', reqOptions);
     request(reqOptions, function(error, response, body) {
-      if(response.statusCode === 200){
-          if(callback && typeof callback === "function"){
-            callback.call(null,body);
-          }
+      if (response.statusCode === 200) {
+        if (callback && typeof callback === "function") {
+          callback.call(null, body);
+        }
       }
     });
   }
 };
-Epo.prototype.published_data = function(options,callback){
+Epo.prototype.published_data = function(options, callback) {
   if (options && options.ref_type && options.format && options.input) {
     options.constituents = options.constituents || "biblio";
     var request = require('request');
@@ -71,23 +79,31 @@ Epo.prototype.published_data = function(options,callback){
       url: urlBuilder.join("/"),
       headers: {
         'Accept': 'application/json',
-        //'Authorization: Bearer '.$token['access_token'],
         'Connection': 'Keep-Alive',
         'Host': 'ops.epo.org',
         'X-Target-URI': 'http://ops.epo.org'
       }
     };
-    if (process.env.DEBUG) console.log('Request to register : ',reqOptions);
+    if (this.token) {
+      if (this.token.expired()) {
+        _this = this;
+        this.token.refresh(function(error, result) {
+          _this.token = result;
+        });
+      }
+      reqOptions["headers"]["Authorization"] = "Bearer " +this.token["token"]["access_token"];
+    }
+    if (process.env.DEBUG) console.log('Request to register : ', reqOptions);
     request(reqOptions, function(error, response, body) {
-      if(response.statusCode === 200){
-          if(callback && typeof callback === "function"){
-            callback.call(null,body);
-          }
+      if (response.statusCode === 200) {
+        if (callback && typeof callback === "function") {
+          callback.call(null, body);
+        }
       }
     });
   }
 };
-Epo.prototype.family = function(options,callback){
+Epo.prototype.family = function(options, callback) {
   if (options && options.ref_type && options.format && options.input) {
     options.constituents = options.constituents || "";
     var request = require('request');
@@ -96,96 +112,126 @@ Epo.prototype.family = function(options,callback){
       url: urlBuilder.join("/"),
       headers: {
         'Accept': 'application/json',
-        //'Authorization: Bearer '.$token['access_token'],
         'Connection': 'Keep-Alive',
         'Host': 'ops.epo.org',
         'X-Target-URI': 'http://ops.epo.org'
       }
     };
-    if (process.env.DEBUG) console.log('Request to register : ',reqOptions);
+    if (this.token) {
+      if (this.token.expired()) {
+        _this = this;
+        this.token.refresh(function(error, result) {
+          _this.token = result;
+        });
+      }
+      reqOptions["headers"]["Authorization"] = "Bearer " +this.token["token"]["access_token"];
+    }
+    if (process.env.DEBUG) console.log('Request to register : ', reqOptions);
     request(reqOptions, function(error, response, body) {
-      if(response.statusCode === 200){
-          if(callback && typeof callback === "function"){
-            callback.call(null,body);
-          }
+      if (response.statusCode === 200) {
+        if (callback && typeof callback === "function") {
+          callback.call(null, body);
+        }
       }
     });
   }
 };
-Epo.prototype.published_data_search = function(options,callback){
-  if(options && options.cql){
+Epo.prototype.published_data_search = function(options, callback) {
+  if (options && options.cql) {
     options.range_begin = options.range_begin || 1;
     options.range_end = options.range_end || 25;
     var request = require('request');
-    var urlBuilder = this.service_url_prefix + "/" + this.published_data_search_path+"?q="+options.cql+"&Range="+options.range_begin+"-"+options.range_end;
+    var urlBuilder = this.service_url_prefix + "/" + this.published_data_search_path + "?q=" + options.cql + "&Range=" + options.range_begin + "-" + options.range_end;
     var reqOptions = {
       url: urlBuilder,
       headers: {
         'Accept': 'application/json',
-        //'Authorization: Bearer '.$token['access_token'],
         'Connection': 'Keep-Alive',
         'Host': 'ops.epo.org',
         'X-Target-URI': 'http://ops.epo.org'
       }
     };
-    if (process.env.DEBUG) console.log('Request to register : ',reqOptions);
+    if (this.token) {
+      if (this.token.expired()) {
+        _this = this;
+        this.token.refresh(function(error, result) {
+          _this.token = result;
+        });
+      }
+      reqOptions["headers"]["Authorization"] = "Bearer " +this.token["token"]["access_token"];
+    }
+    if (process.env.DEBUG) console.log('Request to register : ', reqOptions);
     request(reqOptions, function(error, response, body) {
-      if(response.statusCode === 200){
-          if(callback && typeof callback === "function"){
-            callback.call(null,body);
-          }
+      if (response.statusCode === 200) {
+        if (callback && typeof callback === "function") {
+          callback.call(null, body);
+        }
       }
     });
   }
 };
-Epo.prototype.register_search = function(options,callback){
-  if(options && options.cql){
+Epo.prototype.register_search = function(options, callback) {
+  if (options && options.cql) {
     options.range_begin = options.range_begin || 1;
     options.range_end = options.range_end || 25;
     var request = require('request');
-    var urlBuilder = this.service_url_prefix + "/" + this.register_search_path+"?q="+options.cql+"&Range="+options.range_begin+"-"+options.range_end;
+    var urlBuilder = this.service_url_prefix + "/" + this.register_search_path + "?q=" + options.cql + "&Range=" + options.range_begin + "-" + options.range_end;
     var reqOptions = {
       url: urlBuilder,
       headers: {
         'Accept': 'application/json',
-        //'Authorization: Bearer '.$token['access_token'],
         'Connection': 'Keep-Alive',
         'Host': 'ops.epo.org',
         'X-Target-URI': 'http://ops.epo.org'
       }
     };
-    if (process.env.DEBUG) console.log('Request to register : ',reqOptions);
+    if (this.token) {
+      if (this.token.expired()) {
+        _this = this;
+        this.token.refresh(function(error, result) {
+          _this.token = result;
+        });
+      }
+      reqOptions["headers"]["Authorization"] = "Bearer " +this.token["token"]["access_token"];
+    }
+    if (process.env.DEBUG) console.log('Request to register : ', reqOptions);
     request(reqOptions, function(error, response, body) {
-      if(response.statusCode === 200){
-          if(callback && typeof callback === "function"){
-            callback.call(null,body);
-          }
+      if (response.statusCode === 200) {
+        if (callback && typeof callback === "function") {
+          callback.call(null, body);
+        }
       }
     });
   }
 };
 var myEpo = new Epo({
-  // clientID: '3U0OeLMQOj5pxi0Mqs5oVYM3BrxxmS3P',
-  // clientSecret: 's5qxUm5vnQdUOqJ6'
+  clientID: '3U0OeLMQOj5pxi0Mqs5oVYM3BrxxmS3P',
+  clientSecret: 's5qxUm5vnQdUOqJ6'
 });
-// myEpo.generate_token(function() {
-//   // console.log(this.token);
-//   // console.log(this.read_token());
+myEpo.generate_token(function() {
+  console.log(this.token);
+  myEpo.register_search({
+    cql: "ti%3Dplastic",
+    range_begin: 1,
+    range_end: 30
+  }, function(epo_data) {
+    console.log(epo_data);
+  });
+});
+// myEpo.register({
+//   ref_type: "publication",
+//   format: "epodoc",
+//   input: "EP1122334"
+// }, function(epo_data) {
+//   console.log(JSON.parse(epo_data));
 // });
-myEpo.register({
-  ref_type: "publication",
-  format: "epodoc",
-  input: "EP1122334"
-},function(epo_data){
-  console.log(JSON.parse(epo_data));
-});
-myEpo.register_search({
-  cql: "ti%3Dplastic",
-  range_begin: 1,
-  range_end: 30
-},function(epo_data){
-  console.log(epo_data);
-});
+// myEpo.register_search({
+//   cql: "ti%3Dplastic",
+//   range_begin: 1,
+//   range_end: 30
+// }, function(epo_data) {
+//   console.log(epo_data);
+// });
 
 
 // coonsole.log(myEpo.read_token);
